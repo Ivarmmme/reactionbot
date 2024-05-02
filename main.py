cc57e435eb5633f4b255caf778ae026ea21b1a01
Ivarmmme <162916474+Ivarmmme@users.noreply.github.com>
2024-05-02 13:44:10 +0530

    Update main.py

--- main.py
+++ main.py
@@ -17,35 +17,33 @@ except FileNotFoundError:
     message_counts = defaultdict(int)
 
 async def start(update: Update, context: CallbackContext) -> None:
-    await update.message.reply_text('Hello! I will start tracking messages from specified users.')
+    await update.message.reply_text('Tracking started.')
+    print('Start command issued.')
 
 async def count_messages(update: Update, context: CallbackContext) -> None:
-    # Check if the update contains a message
     if update.message:
         user_id = update.message.from_user.id
         if user_id in SPECIFIED_USERS:
             message_counts[user_id] += 1
-            # Save the updated counts to the file
             with open(COUNTS_FILE, 'w') as file:
-                # Convert defaultdict to a regular dict for JSON serialization
                 json.dump(dict(message_counts), file)
 
 async def rankings(update: Update, context: CallbackContext) -> None:
+    print('Rankings command issued.')
     ranked_users = [(user_id, count) for user_id, count in message_counts.items() if user_id in SPECIFIED_USERS]
     ranked_users.sort(key=lambda item: item[1], reverse=True)
     
-    ranking_text = 'Message Rankings (Specified Users):\n'
+    ranking_text = 'Message Rankings:\n'
     for user_id, count in ranked_users:
         ranking_text += f'User {user_id}: {count} messages\n'
     
     await update.message.reply_text(ranking_text)
 
 async def error_handler(update: object, context: CallbackContext) -> None:
-    # Log the error
     print(f"An error occurred: {context.error}")
 
 def main() -> None:
-    application = Application.builder().token('7116973701:AAHkNxvQqS85ko0sBtQ_QoVM90UKfzAOgco').build()
+    application = Application.builder().token('7113474118:AAF0aoGSb3BDGcA5XfvhvHH3p9LuYFymzQ0').build()
 
     start_handler = CommandHandler('start', start)
     count_handler = MessageHandler(filters.TEXT & filters.ChatType.GROUPS, count_messages)
@@ -55,7 +53,6 @@ def main() -> None:
     application.add_handler(count_handler)
     application.add_handler(rankings_handler)
 
-    # Register the error handler
     application.add_error_handler(error_handler)
 
     application.run_polling()
